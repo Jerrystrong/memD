e00e2ffc18c51ae80ba763160da7ead95d626f4d
Jeremie Kimpioka <jeremiekimpioka52@gmail.com>
2024-10-10 18:00:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// configuration tailwind
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primaryColor: '#001F3F',
+                secondColor: '#6A9AB0',
+                whiteColor:'#FEFEFE'
+            },
+            spacing:{
+                '1': '8px',
+                '2': '12px',
+                '3': '16px',
+                '4': '24px',
+                '5': '32px',
+                '6': '48px',
+                '7':'52px',
+                '10':'100px',
+                '12':'120px',
+                '15':'225px'
+              },fontSize: {
+                bigText:'64px',
+              }, blur: {
+                xs: '2px',
+              }
+        }
+    }
+}
